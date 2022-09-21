ca95c1ad1b228890e8053d2bf5116d518a5b88a4
Karen Douglas <kdindependence@gmail.com>
2022-09-21 13:55:40 -0500

    creating grid system

--- starter-code/gulpfile.js
+++ starter-code/gulpfile.js
@@ -1,13 +1,13 @@
 const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const purgecss = require('gulp-purgecss')
+//const purgecss = require('gulp-purgecss')
 
 //function to compile sass into css
 function buildStyles(){
     // specifying source and destination of output
     return src('scss/**/*.scss')
         .pipe(sass())
-        .pipe(purgecss({content: ['*.html']}))
+       // .pipe(purgecss({content: ['*.html']}))
         .pipe(dest('css'))
 }
 
